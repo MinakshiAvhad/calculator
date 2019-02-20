b6527dee9488d511038bf2e630119a16978db7a2
Minakshi Avhad <minakshi.avhad@prohaktiv.com>
2019-02-20 09:38:35 +0530

    added all files

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '/../public');
+const express = require('express');
+console.log(__dirname + '/../public');
+console.log(publicPath);
+var app = express();
+app.use(express.static(publicPath));
+app.listen(3000, () => {
+    console.log("SERVER is up");
+})
